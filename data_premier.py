8ab33adb52db1159effb3a0f74a4a117a8057783
guille <guille08mart@gmail.com>
2025-03-15 12:15:00 +0100

    Updated raw data

--- data_premier.py
+++ data_premier.py
@@ -20,35 +20,35 @@ stats_shooting = pd.concat([
     fbref.read_team_match_stats(stat_type="shooting", opponent_stats=True, team=team).assign(team=team)
     for team in teams
 ], ignore_index=True)
-stats_shooting.to_csv("Premier_League_Shooting.csv", index=False)
+stats_shooting.to_csv("RawDataShooting.csv", index=False)
 print("Shooting data saved to csv")
 
 stats_passing = pd.concat([
     fbref.read_team_match_stats(stat_type="passing", opponent_stats=True, team=team).assign(team=team)
     for team in teams
 ], ignore_index=True)
-stats_passing.to_csv("Premier_League_Passing.csv", index=False)
+stats_passing.to_csv("RawDataPassing.csv", index=False)
 print("Passing data saved to csv")
 
 stats_passing_types = pd.concat([
     fbref.read_team_match_stats(stat_type="passing_types", opponent_stats=True, team=team).assign(team=team)
     for team in teams
 ], ignore_index=True)
-stats_passing_types.to_csv("Premier_League_Passing_Types.csv", index=False)
+stats_passing_types.to_csv("RawDataPassingTypes.csv", index=False)
 print("Passing Types data saved to csv")
 
 stats_possession = pd.concat([
     fbref.read_team_match_stats(stat_type="possession", opponent_stats=True, team=team).assign(team=team)
     for team in teams
 ], ignore_index=True)
-stats_possession.to_csv("Premier_League_Posession.csv", index=False)
+stats_possession.to_csv("RawDataPosession.csv", index=False)
 print("Possession data saved to csv")
 
 stats_misc = pd.concat([
     fbref.read_team_match_stats(stat_type="misc", opponent_stats=True, team=team).assign(team=team)
     for team in teams
 ], ignore_index=True)
-stats_misc.to_csv("Premier_League_Misc.csv", index=False)
+stats_misc.to_csv("RawDataMisc.csv", index=False)
 print("Misc data saved to csv")
 
 
@@ -56,12 +56,12 @@ stats_defense = pd.concat([
     fbref.read_team_match_stats(stat_type="defense", opponent_stats=True, team=team).assign(team=team)
     for team in teams
 ], ignore_index=True)
-stats_defense.to_csv("Premier_League_Defense.csv", index=False)
+stats_defense.to_csv("RawDataDefense.csv", index=False)
 print("Defense data saved to csv")
 
 # stats_schedule = pd.concat([
 #     fbref.read_team_match_stats(stat_type="schedule", opponent_stats=True, team=team).assign(team=team)
 #     for team in teams
 # ], ignore_index=True)
-# stats_schedule.to_csv("Premier_League_Schedule.csv", index=False)
+# stats_schedule.to_csv("RawDataSchedule.csv", index=False)
 # print("Schedule data saved to csv")
